3dbe496d84d94def31b0413e92dc9dc13174156b
Rebakum <anamikaeasmin@hotmail.com>
2024-08-03 21:33:38 +0600

    add:wishlist page added

---
+++ src/Pages/WishList/WishList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WishList = () => {
+    return (
+        <div>
+           <h1>This is WishList Page</h1> 
+        </div>
+    );
+};
+
+export default WishList;
