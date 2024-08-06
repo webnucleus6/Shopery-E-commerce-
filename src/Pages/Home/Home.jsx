ffd78be31c4994aa003b19e72e106e2176c3a4c8
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-08-06 14:16:45 +0600

    update: offer banner added without responsive

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,28 +4,42 @@ import HomepageServices from './HomeComponents/HomepageServices';
 import HomePopularCategories from './HomeComponents/HomePopularCategories';
 import PopularProducts from './HomeComponents/PopularProducts';
 import OfferSlider from './HomeComponents/OfferSlider';
+import CommonButton from '../../Shared/CommonButton/CommonButton';
 
 const Home = () => {
     return (
         <div className='all-container mx-auto mt-6'>
 
             {/* Banner component */}
-            <Banner/>
+            <Banner />
 
             {/* Services banner */}
-            <HomepageServices/>
+            <HomepageServices />
 
             {/* Popular categories */}
-            <HomePopularCategories/>
+            <HomePopularCategories />
 
 
             {/* Popular Products */}
-            <PopularProducts/>
+            <PopularProducts />
 
 
             {/* Offer slides */}
-            <OfferSlider/>
-            
+            <OfferSlider />
+
+
+
+            {/* Offer Card Banner */}
+            <div className='bg-[url("https://i.postimg.cc/76c4FyGL/offercard.png")] min-h-[360px] bg-no-repeat bg-contain mt-16 flex justify-end items-center pr-16'>
+                <div className='space-y-2'>
+                    <p className='uppercase font-semibold text-white'>Summer Sale</p>
+                    <h2 className='text-[56px] text-white'><span className='font-bold text-[#ff8a00]'>37%</span> OFF</h2>
+                    <p className='footer-text'>Free on all your order, Free Shipping and  30 <br /> days money-back guarantee</p>
+
+                    <CommonButton btnText={"Shop Now"} customStyle={"bg-[#0bb419] text-white "}/>
+                </div>
+            </div>
+
         </div>
     );
 };
