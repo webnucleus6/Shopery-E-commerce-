265aaa4878062dc06bf24ad21eeef65b02fa14d3
Rebakum <anamikaeasmin@hotmail.com>
2024-08-07 12:55:21 +0600

    added shopping cart wishlist or images

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,12 +2,22 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
+import ShoppingCart from "../Pages/ShoppingCart/ShoppingCart";
+import WishList from "../Pages/WishList/WishList";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+  },
+  {
+    path: "/wishList",
+    element: <WishList />,
+  },
+  {
+    path: "/shopping",
+    element: <ShoppingCart />,
   },
 ]);
 export default router;
