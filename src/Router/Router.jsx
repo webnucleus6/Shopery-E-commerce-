c16daa96f761e06d4312b317f61836134db4c686
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-08-07 20:01:28 +0600

    create : About Page

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,12 +2,17 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
+import About from "../Pages/About/About";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+  },
+  {
+    path: "/about",
+    element:  <About/> ,
   },
 ]);
 export default router;
