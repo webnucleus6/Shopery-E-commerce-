d3e5acbfacec6b6b53a3cf1845b4d9bc528f76c1
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-08-29 01:52:17 +0600

    Added image laizy loading and loading skeleton

--- src/Pages/Home/HomeComponents/PopularProducts.jsx
+++ src/Pages/Home/HomeComponents/PopularProducts.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import ProductLoader from '../../../Shared/Loaders/ProductLoader/ProductLoader';
-import { FaArrowRight } from 'react-icons/fa';
+import { FaArrowRight, FaRegHeart } from 'react-icons/fa';
 import Rating from 'react-rating';
 import { SlHandbag } from "react-icons/sl";
 
@@ -8,12 +8,16 @@ import { SlHandbag } from "react-icons/sl";
 import starGray from '../../../assets/icons/star-grey.png';
 import starRed from '../../../assets/icons/star-red.png';
 import starYellow from '../../../assets/icons/star-yellow.png';
+import { Link } from 'react-router-dom';
+import { LuEye } from 'react-icons/lu';
 
 
 
 const PopularProducts = () => {
     const [allProducts, setAllProducts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [isHoverId, setIsHoverId] = useState(null);
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
 
     useEffect(() => {
         setLoading(true);
@@ -37,45 +41,47 @@ const PopularProducts = () => {
                 <button className='flex items-center gap-3 text-[#00b207] hover:underline'>View all <FaArrowRight /></button>
 
             </div>
-            <div className='grid grid-cols-5'>
-                {
-                    allProducts?.map(({ productPic, productId, name, offerPrice, regularPrice, ratings,discount }) =>
-
-                        <div
-                            key={productId}
-                            className='border relative  py-2  cursor-pointer hover:border-[#2C742F] hover:shadow-lg hover:text-[#2C742F]'
-                        >
-                            <img src={productPic} alt="" />
-
-                            <div className='flex justify-between items-center px-4'>
-                                <div>
-
-                                    <h2 className=' font-semibold text-[#4d4d4d]'>{name}</h2>
-                                    <div className='flex gap-2'>
-                                        <h2 className={`${offerPrice ? "font-bold text-xl" : "hidden"}`}>$ {offerPrice} </h2>
-                                        <h2 className={`${offerPrice ? "line-through text-[#999999] text-base" : "font-bold text-xl"} `}>$ {regularPrice} </h2>
-
-                                    </div>
-                                    <Rating
-                                        placeholderRating={ratings}
-                                        emptySymbol={<img src={starGray} className="icon" />}
-                                        placeholderSymbol={<img src={starRed} className="icon" />}
-                                        fullSymbol={<img src={starYellow} className="icon" />}
-                                    />
-
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5'>
+                {allProducts?.map(({ productPic, productId, name, offerPrice, regularPrice, ratings, discount, brandName }) =>
+                    <div
+                        onMouseOver={() => setIsHoverId(productId)}
+                        onMouseOut={() => setIsHoverId(null)}
+                        key={productId}
+                        className='border relative py-2 cursor-pointer hover:border-[#2C742F] hover:shadow-lg hover:text-[#2C742F]'
+                    >
+                        {!isImageLoaded && <div className="skeleton h-40 w-48 mx-auto"></div>}
+                        <img
+                            src={productPic}
+                            alt={name}
+                            className={isImageLoaded ? '' : 'hidden'}
+                            onLoad={() => setIsImageLoaded(true)}
+                        />
+                        <div className='flex justify-between items-center px-4'>
+                            <div>
+                                <Link to={`/details/${productId}`} className='font-semibold text-[#00B207] hover:text-[#4d4d4d] hover:underline'>{name}</Link>
+                                <p className='text-xs text-[#4d4d4d]'>{brandName}</p>
+                                <div className='flex gap-2'>
+                                    {offerPrice && <h2 className='font-semibold text-xl'>$ {offerPrice}</h2>}
+                                    <h2 className={offerPrice ? "line-through text-[#999999] text-base" : "font-semibold text-xl"}>$ {regularPrice}</h2>
                                 </div>
-                                <div className='bg-[#f2f2f2] p-3 rounded-full hover:bg-[#00b207] hover:text-white transition'>
-                                    <SlHandbag />
-                                </div>
-
+                                <Rating
+                                    placeholderRating={ratings}
+                                    emptySymbol={<img src={starGray} className="icon" alt="gray star" />}
+                                    placeholderSymbol={<img src={starRed} className="icon" alt="red star" />}
+                                    fullSymbol={<img src={starYellow} className="icon" alt="yellow star" />}
+                                />
                             </div>
-
-                            <p  className={`${discount?"px-2 py-1 bg-[#ea4b48] w-fit rounded-md text-white absolute top-4 left-4":"hidden"}`}>Sale {discount}%</p>
-
+                            <div className='bg-[#f2f2f2] p-3 rounded-full hover:bg-[#00b207] hover:text-white transition'>
+                                <SlHandbag />
+                            </div>
                         </div>
-                    )
-                }
-
+                        {discount && <p className='px-2 py-1 bg-[#ea4b48] w-fit rounded-md text-white absolute top-4 left-4'>Sale {discount}%</p>}
+                        <div className={`absolute top-3 right-3 space-y-2 ${isHoverId === productId ? 'block' : 'hidden'}`}>
+                            <div className='p-3 rounded-full border border-[#f2f2f2] bg-white hover:bg-[#00b207] hover:text-white transition'><FaRegHeart /></div>
+                            <div className='p-3 rounded-full border border-[#f2f2f2] bg-white hover:bg-[#00b207] hover:text-white transition'><LuEye /></div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
