9acd65e35782e78d9ac0675106337e2555933312
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-08-29 01:13:53 +0600

    Fixed all issue and optimize some code and remove unnesessary code

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -6,29 +6,20 @@ import NavBar from "../../Shared/Navbar/NavBar";
 
 const Error = () => {
     return (
-        <div >
-            <div className="error">
-                 <NavBar/>
-                <div>
-                    <div>
-                    <Nav_Banner name={'Error'} /> 
-                    </div>
-                    <div className="flex flex-col items-center mt-10 px-4 md:p-0">
-                        <div className="w-full md:w-1/3">
-                            <img className="w-full" src="/Illustration.png" alt="" />
-                        </div>
-                        <div className="content text-center space-y-4">
-                            <h2 className="font-semibold md:text-6xl text-3xl">Oops! page not found</h2>
-                            <p className="text-[#808080] text-base">
-                            Ut consequat ac tortor eu vehicula. Aenean accumsan purus eros. <br />   Maecenas sagittis tortor at metus mollis
-                            </p>
-                            <Link to="/" className="btn bg-[#00B207] text-white   rounded-full">Back to Home</Link>
-                        </div>
-                    </div>
-                </div>
-                <Footer></Footer>
+
+        <div className="flex flex-col items-center justify-center mt-10 px-4 md:p-0 h-[90vh]">
+            <div className="w-full md:w-1/3">
+                <img className="w-full" src="/Illustration.png" alt="" />
+            </div>
+            <div className="content text-center space-y-4">
+                <h2 className="font-semibold md:text-6xl text-3xl">Oops! page not found</h2>
+                <p className="text-[#808080] text-base">
+                    Ut consequat ac tortor eu vehicula. Aenean accumsan purus eros. <br />   Maecenas sagittis tortor at metus mollis
+                </p>
+                <Link to="/" className="btn bg-[#00B207] text-white   rounded-full">Back to Home</Link>
             </div>
         </div>
+
     );
 };
 
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -5,10 +5,10 @@ import Home from "../Pages/Home/Home";
 import About from "../Pages/About/About";
 import Shop from "../Pages/Shop/Shop";
 import ProductDetails from "../Pages/ProductDetails/ProductDetails";
- 
+
 import Dashboard from "../Pages/Dashboard/Dashboard";
 import UserHome from "../Pages/Dashboard/UserPages/UserHome";
- 
+
 import BillingInfo from "../Pages/BillingInfo/BillingInfo";
 import ContactPage from "../Pages/ContactPage/contactPage";
 import ShoppingCart from "../Pages/ShoppingCart/ShoppingCart";
@@ -16,22 +16,22 @@ import WishList from "../Pages/WishList/WishList";
 import Error from "../Pages/Error/Error";
 import LogIn from "../Pages/LogIn/LogIn";
 import Register from "../Pages/Register/Register";
- 
- 
 
- 
 
- 
+
+
+
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    errorElement:<Error></Error>,
- 
+    errorElement: <Error></Error>,
+
     children: [
-     {
-        path:"/",
-        element:<Home/>
+      {
+        path: "/",
+        element: <Home />
       },
       {
         path: "/shop",
@@ -41,61 +41,54 @@ const router = createBrowserRouter([
         path: "/product-details",
         element: <ProductDetails />,
       },
- 
       {
-        path: "/dashboard",
-        element: <Dashboard />,
-        children: [
-          {
-            path: "user",
-            element: <UserHome />,
-          },
-        ],
-      },
-    ],
- 
- 
-       {path: "/wishList",
+        path: "/wishList",
         element: <WishList />,
       },
       {
         path: "/shopping-cart",
         element: <ShoppingCart />,
       },
- 
-   {
-    path: "/about",
-    element:  <About/> ,
-  },
- 
-  {
-    path:'/login',
-    element:<LogIn></LogIn>
- 
-  },
-  {
-    path:'/register',
-    element:<Register></Register>
-  },
- 
- 
+
+      {
+        path: "/about",
+        element: <About />,
+      },
+
+      {
+        path: '/login',
+        element: <LogIn/>
+
+      },
+      {
+        path: '/register',
+        element: <Register/>
+      },
       {
         path: "/billing-info",
         element: <BillingInfo />
       },
-  
+
       {
         path: "/contact-us",
-        element: <ContactPage/>,
+        element: <ContactPage />,
       },
- 
- 
 
- 
-    ]
+    ],
+
   },
- 
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "user",
+        element: <UserHome />,
+      },
+    ],
+  },
+
 ]);
 export default router;
 
- 
+
